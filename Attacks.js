fe2c4fc39fa4d3fa423bc517cc9cded257634211
Emil Bakhshiev <emilbahshiev@gmail.com>
2021-01-24 13:51:32 +0300

    feat: сделать боевку(нет)

--- Attacks.js
+++ Attacks.js
@@ -1,22 +1,22 @@
 export default class Attacks{
-    constructor(data, attacksSelector){
-        this._attacks = data.attacks;
-        this._attacksSelector = attacksSelector;
-    }
-    _getTemplate(){
-        const buttonAttack = document
-        .querySelector(this._attacksSelector)
-        .content
-        .cloneNode(true);
-
-        return buttonAttack;
-    }
-    renderAttacks(){
-        this._element = this. _getTemplate();
-        //this._setEventListeners();
-        this._element.querySelector('.button').textContent = this._attacks[0].name;
-        
-        return this._element;
+    constructor(character, enemy){
+        this._minCharacterDamage = character._attacks[0].minDamage;
+        this._maxCharacterDamage = character._attacks[0].maxDamage;
+        this._minCharacterDamage = enemy._attacks[0].minDamage;
+        this._maxCharacterDamage = enemy._attacks[0].maxDamage;
+        this._healthCharacter = character.health;
+        this._healthEnemy = enemy.health;
+        this._enemyHelthAfterAttack = enemy.healthAfterAttack;
+        this._damage =  this.damage();
+        //this._healthAfterDamage = this.healthAfterDamage();
+        //this._attacksSelector = attacksSelector;
     }
 
+    damage(){
+        return Math.floor(Math.random() * (this._maxCharacterDamage - this._minCharacterDamage) + this._minCharacterDamage);  
+    }
+
+    attackHero(){
+        return this._enemyHelthAfterAttack  =  this._healthEnemy - this._damage;
+    }
 }
--- main.js
+++ main.js
@@ -5,72 +5,6 @@ import {ninja} from './ninja.js';
 const playground = document.querySelector('.playground');
 const randomButton = document.querySelector('#randomCharacters')
 
- const ninjaDataOne =  {
-  name: 'Naruto',
-  type: 'unknown',
-  hp: 564,
-  img: 'https://res.cloudinary.com/digaox5o1/image/upload/v1611014924/naruto_jrwlvd.png',
-  attacks: [
-      {
-          name: "Clone Lasso",
-          maxDamage: 40,
-          minDamage: 20,
-          maxCount: 100,
-      },
-      {
-          name: "Rasengan",
-          maxDamage: 60,
-          minDamage: 45,
-          maxCount: 6,
-      },
-      {
-          name: "Rasenshuriken",
-          maxDamage: 75,
-          minDamage: 60,
-          maxCount: 4,
-      },
-      {
-          name: "Twin Rasenshuriken",
-          maxDamage: 160,
-          minDamage: 130,
-          maxCount: 2,
-      }
-  ]
-}
-
-const nanjaDataTwo = {
-  img: 'https://res.cloudinary.com/digaox5o1/image/upload/v1611014890/gaara_pnzqeo.png',
-  name: 'Gaara',
-  type: 'unknown',
-  hp: 282,
-  attacks: [
-      {
-          name: "Sand Shield",
-          maxDamage: 40,
-          minDamage: 20,
-          maxCount: 100,
-      },
-      {
-          name: "Sand Swords",
-          maxDamage: 60,
-          minDamage: 45,
-          maxCount: 6,
-      },
-      {
-          name: "Sand Shuriken",
-          maxDamage: 75,
-          minDamage: 60,
-          maxCount: 4,
-      },
-      {
-          name: "Sand Clone",
-          maxDamage: 130,
-          minDamage: 110,
-          maxCount: 2,
-      }
-  ]
-}
-
 
 function randomCharacter(ninja){
    const randomValue = ninja[Math.floor(ninja.length * Math.random())];
@@ -79,15 +13,18 @@ function randomCharacter(ninja){
 
   const character = new Character(randomCharacter(ninja), '#hero');
   const characterCard = character.renderCharacterCard();
-playground.prepend(characterCard);
+  let buttonAtack = character.renderCharacterCard().querySelector('.button');
+  playground.prepend(characterCard);
 
   const enemy = new Character(randomCharacter(ninja), '#enemy');
   const enemyCard = enemy.renderCharacterCard();
+  let hpEnemy = enemy.renderCharacterCard().querySelector('#basisHealth');
   playground.append(enemyCard);
 
-/*
-const heroAttack = new Attacks(ninjaDataOne, '#hero')
-const buttonAttack = heroAttack.renderAttacks();
-playground.prepend(buttonAttack);*/
+  const attack = new Attacks(character, enemy);
+  buttonAtack.addEventListener('click', ()=>{
+    hpEnemy.textContent = character.damageforEnemy(attack.attackHero(enemy));
+  })
+
 
 
